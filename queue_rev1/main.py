cb769dfe4f229de47a0fd22b1e47aba3c1df34e4
aarskog <eparis@atlassian.com>
2016-02-26 14:04:51 +0100

    test for å finne bug

--- queue_rev1/main.py
+++ queue_rev1/main.py
@@ -11,44 +11,47 @@ mutexKey = Lock()
 def set_floor():
 	goFloor = 1
 	while True:
-		mutexKey.acquire()
+		#mutexKey.acquire()
 		for x in range(0,constants.N_FLOORS):
 			if (x in queue) and queue[x] == 1:
 				if lastFloor == goFloor:
 					goFloor = x
+					print (goFloor)
 				elif (x < goFloor) and (lastFloor < goFloor):
 					goFloor = x
+					print (goFloor)
 				elif (x > goFloor) and (lastFloor > goFloor):
 					goFloor = x
-		mutexKey.release()
+					print (goFloor)
+		#mutexKey.release()
 
 		elevator.go_to_floor(goFloor)
 
 		lastFloor = elevator.last_floor()
 		if (lastFloor == goFloor):
-			mutexKey.acquire()
+			#mutexKey.acquire()
 			queue[goFloor] = 0
-			mutexKey.release()
+			#mutexKey.release()
 
-		mutexKey.acquire()	
-		print (queue)
-		mutexKey.release()
+		#mutexKey.acquire()	
+		#print (queue)
+		#mutexKey.release()
 
 
 def set_queue():
 
-	mutexKey.acquire()
+	#mutexKey.acquire()
 	for x in range(0, constants.N_FLOORS):
 		queue[x] = 0
 	print queue
-	mutexKey.release()
+	#mutexKey.release()
 	while True:
 		
 		nextFloor = panel.read_buttons()
 		if nextFloor >= 0:
-			mutexKey.acquire()
+			#mutexKey.acquire()
 			queue[nextFloor]=1
-			mutexKey.release()
+			#mutexKey.release()
 
 
 
