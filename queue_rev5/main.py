78c088d30ff31e663861ef6c9a65c524b23ae6d6
HAB <kissahen@gmail.com>
2016-03-04 14:01:41 +0100

    tilsynelatende fungerende modul mangler å gjøres om til classe

--- queue_rev5/main.py
+++ queue_rev5/main.py
@@ -8,64 +8,53 @@ from threading import Lock
 elevator = ElevInterface.ElevInterface()
 panel = PanelInterface.PanelInterface()
 
-queueKey = Lock()
-queue = [[0 for y in range(0,3)] for x in range(0,constants.N_FLOORS)]
-goFloor = 2
-lastFloor = 2
+queue_key = Lock()
+queue = [[0 for button in range(0,3)] for floor in range(0,constants.N_FLOORS)]
 
 
-def queue_floor():
-	global goFloor
-	global lastFloor
+def run_floor_queue():
+	last_floor=2
+	next_floor=2
 	direction = "None"
 	while True:
 		time.sleep(0.001)
-		x_max = 0
-		x_min = constants.N_FLOORS-1
 
-		for x in range(0,constants.N_FLOORS):
-			for y in range(0,3):
-				if queue[x][y] == 1:
-					x_max = max(x_max,x)
-					x_min = min(x_min,x)
-					if (lastFloor == goFloor) and (direction != "DOWN") and (goFloor < x_max):
-						goFloor = x
+		floor_max = 0
+		floor_min = constants.N_FLOORS-1
+
+		for floor in range(0,constants.N_FLOORS):
+			for button in range(0,3):
+				if queue[floor][button] == 1:
+					floor_max = max(floor_max,floor)
+					floor_min = min(floor_min,floor)
+					if (last_floor == next_floor) and (direction != "DOWN") and (next_floor < floor_max):
+						next_floor = floor
 						direction = "UP"
-					elif (lastFloor == goFloor) and (direction != "UP") and (goFloor > x_min):
-						goFloor = x
+					elif (last_floor == next_floor) and (direction != "UP") and (next_floor > floor_min):
+						next_floor = floor
 						direction = "DOWN"
-					elif (lastFloor < goFloor) and (x < goFloor) and (x > lastFloor) and (y!=1):
-						goFloor = x
-					elif (lastFloor > goFloor) and (x > goFloor) and (x < lastFloor) and (y!=0):
-						goFloor = x
+					elif (last_floor < next_floor) and (floor < next_floor) and (floor > last_floor) and (button != 1):
+						next_floor = floor
+					elif (last_floor > next_floor) and (floor > next_floor) and (floor < last_floor) and (button != 0):
+						next_floor = floor
 
-		if (direction == "UP") and (x_max <= lastFloor):
+		if (direction == "UP") and (floor_max <= last_floor):
 			direction = "None"
-		elif (direction == "DOWN") and (x_min >= lastFloor):
+		elif (direction == "DOWN") and (floor_min >= last_floor):
 			direction = "None"
 
-		checkFloor = elevator.get_floor_sensor_signal()
-		if checkFloor >= 0:
-			lastFloor = checkFloor
+		read_floor = elevator.get_floor_sensor_signal()
+		if read_floor >= 0:
+			last_floor = read_floor
 
+		set_indicator(last_floor,next_floor)
+		go_to_floor(last_floor,next_floor)
+		clear_floor_queue(last_floor,next_floor,direction)
+		
 		print queue
-		if (lastFloor == goFloor):
-			if (direction == "None"):
-				with(queueKey):
-					queue[goFloor][0] = 0
-					queue[goFloor][1] = 0
-					queue[goFloor][2] = 0
-			elif (direction == "UP"):
-				with(queueKey):
-					queue[goFloor][0] = 0
-					queue[goFloor][2] = 0
-			elif (direction == "DOWN"):
-				with(queueKey):
-					queue[goFloor][1] = 0
-					queue[goFloor][2] = 0
 
 
-def read_buttons_to_queue():
+def run_build_queue():
 	while True:
 		time.sleep(0.001)
 		for floor in range (0,constants.N_FLOORS):
@@ -73,59 +62,63 @@ def read_buttons_to_queue():
 				if (floor == 0 and button == 1) or (floor == 3 and button == 0):
 					pass
 				elif panel.get_button_signal(button,floor):
-					with(queueKey):
+					with queue_key:
 						queue[floor][button]=1
 
-def indicators():
-	while True:
-		time.sleep(0.001)
-		for floor in range(0,constants.N_FLOORS):
-				for button in range(0,3):
-						if (floor == 0 and button == 1) or (floor == 3 and button == 0):
-							pass
-						elif queue[floor][button] == 1:
-							panel.set_button_lamp(button,floor,1)
-						else:
-							panel.set_button_lamp(button,floor,0)
-		
-		if lastFloor == goFloor:
-			panel.set_door_open_lamp(1)
-		else:
-			panel.set_door_open_lamp(0)
-
-		panel.set_floor_indicator(lastFloor)
+def clear_floor_queue(last_floor,next_floor,direction):
+	if (last_floor == next_floor):
+		if (direction == "None"):
+			with(queue_key):
+				queue[next_floor][0] = 0
+				queue[next_floor][1] = 0
+				queue[next_floor][2] = 0
+		elif (direction == "UP"):
+			with(queue_key):
+				queue[next_floor][0] = 0
+				queue[next_floor][2] = 0
+		elif (direction == "DOWN"):
+			with(queue_key):
+				queue[next_floor][1] = 0
+				queue[next_floor][2] = 0
 
 
-def go_to_floor():
-	while True:
-		time.sleep(0.001)
-		if lastFloor >= 0:
-			if lastFloor == goFloor:
-				elevator.set_motor_direction(constants.DIRN_STOP)
-				time.sleep(1)
-			elif lastFloor < goFloor:
-				elevator.set_motor_direction(constants.DIRN_UP)
-			elif lastFloor > goFloor:
-				elevator.set_motor_direction(constants.DIRN_DOWN)
+
+def set_indicator(last_floor,next_floor):
+	for floor in range(0,constants.N_FLOORS):
+			for button in range(0,3):
+					if (floor == 0 and button == 1) or (floor == 3 and button == 0):
+						pass
+					elif queue[floor][button] == 1:
+						panel.set_button_lamp(button,floor,1)
+					else:
+						panel.set_button_lamp(button,floor,0)
+	
+	if last_floor == next_floor:
+		panel.set_door_open_lamp(1)
+	else:
+		panel.set_door_open_lamp(0)
+
+	panel.set_floor_indicator(last_floor)
+
+
+def go_to_floor(last_floor,next_floor):
+	if last_floor == next_floor:
+		elevator.set_motor_direction(constants.DIRN_STOP)
+		time.sleep(1)
+	elif last_floor < next_floor:
+		elevator.set_motor_direction(constants.DIRN_UP)
+	elif last_floor > next_floor:
+		elevator.set_motor_direction(constants.DIRN_DOWN)
 
 
 
 
 def main():
-	thread_1 = Thread(target = queue_floor, args = (),)
-	thread_2 = Thread(target = read_buttons_to_queue, args = (),)
-	thread_3 = Thread(target = indicators, args = (),)
-	thread_6 = Thread(target = go_to_floor, args = (),)
+	thread_1 = Thread(target = run_floor_queue, args = (),)
+	thread_2 = Thread(target = run_build_queue, args = (),)
 
 	thread_1.start()
 	thread_2.start()
-	thread_3.start()
-	thread_6.start()
-
-	thread_1.join()
-	thread_2.join()
-	thread_3.join()
-	thread_6.join()
 
 
 if __name__ == "__main__":
