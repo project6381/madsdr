fe774c51bf57584c46a54ef1dec4fc5780fb607a
Student <you@example.com>
2016-03-11 13:42:28 +0100

    queue_elevator -> queue_elevator_run

--- driver/main.py
+++ driver/main.py
@@ -6,7 +6,7 @@ def main():
 
 		(master_floor, master_button) = __Driver.pop_floor_panel_queue()
 		if (master_floor != None) and (master_button != None):
-			__Driver.queue_elevator(master_floor, master_button)
+			__Driver.queue_elevator_run(master_floor, master_button)
 		print __Driver.read_position()
 
 
