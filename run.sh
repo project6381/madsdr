9538fed51502d66aebe9346ec34cf31fcf272ad2
ingvildforseth <ingvild-f@hotmail.com>
2016-03-16 10:58:07 +0100

    made stupid start scrips again. Sorry guys :(

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+COMMAND="python main.py"
+LOGFILE=restart.txt
+
+writelog() {
+  now=`date`
+  echo "$now $*" >> $LOGFILE
+}
+
+writelog "Starting"
+while true ; do
+  $COMMAND
+  writelog "Exited with status $?"
+  writelog "Restarting"
+done
