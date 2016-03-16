0c989f91ba1e8f4657b5c43d6df94581ad7593bd
Einar <einararbo@gmail.com>
2016-03-16 16:40:22 +0100

    exception handling

--- driver/main.py
+++ driver/main.py
@@ -1,18 +1,26 @@
 from driver import Driver
 import time
+import subprocess
+import sys
 
 def main():
 	try:
-		time.sleep(3)
+		#time.sleep(3)
 		driver = Driver()
 		while True:
 			(master_floor, master_button) = driver.pop_floor_panel_queue()
 			if (master_floor != None) and (master_button != None):
 				driver.queue_elevator_run(master_floor, master_button)
-			print driver.read_position()
-	except:
-		driver.stop()
-		execfile("main.py")
+			#print driver.read_position()
+
+	except KeyboardInterrupt:
+		pass
+	except StandardError as error:
+		print error
+		print "standard error"
+	finally:
+		print "exiting main"
+		
 
 if __name__ == "__main__":
 		main()
---
+++ driver/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+COMMAND="python main.py"
+LOGFILE=run_log.txt
+
+writelog() {
+  now=`date`
+  echo "$now $*" >> $LOGFILE
+}
+
+writelog "Starting"
+while true ; do
+  $COMMAND
+  writelog "Exited with status $?"
+  writelog "Restarting"
+done
