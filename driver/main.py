0fa135aaf3a63b73ec724c38814006cea8bd3bc8
Abortkleshengeren <sales@scrapeitout.com>
2016-04-05 13:59:16 +0200

    testfunksjon for oppdatert slave_driver

--- driver/main.py
+++ driver/main.py
@@ -1,16 +1,27 @@
 from slave_driver import SlaveDriver
+from constants import N_FLOORS
 import time
 import subprocess
 import sys
+from random import randint
+
+
 
 def main():
+	master_queue = [0 for floor in range(0,N_FLOORS*2)]
 	try:
 		#time.sleep(3)
 		driver = SlaveDriver()
 		while True:
 			(master_floor, master_button) = driver.pop_floor_panel_queue()
 			if (master_floor != None) and (master_button != None):
-				driver.queue_elevator_run(master_floor, master_button)
+				if master_button = 0:
+					master_queue[master_floor] = randint(1,3)
+				if master_button = 1:
+					master_queue[master_floor] = randint(1,3)
+				driver.master_queue_elevator_run(master_queue)
+
+			print driver.read_saved_master_queue()
 			#print driver.read_position()
 
 	except KeyboardInterrupt:
